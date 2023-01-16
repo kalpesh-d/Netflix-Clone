9e222e1ce8ccbae9642451144ccc450277c22b02
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-01-16 21:11:17 +0530

    faq input done for mobile but not for desktop

--- collapse.js
+++ collapse.js
@@ -3,11 +3,12 @@ let coll = document.getElementsByClassName("header");
 for(let i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
-
+    
         let content = this.nextElementSibling;
 
         if (content.style.display == "block"){
             content.style.display = "none";
+            icon.sty
         } else {
             content.style.display = "block";
         }
